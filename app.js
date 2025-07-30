6629b81135c15a4e7c3744945aeda539f24f3e1b
brayancastano <50753179+brayancastano@users.noreply.github.com>
2025-07-30 10:21:05 -0500

    Update 2 app.js

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-// Importar Express.js const express = require ( 'express' );
+// Importar Express.js
+const express = require('express');
 
+// Crea una aplicación Express
+const app = express();
 
-// Crea una aplicación Express const app = express ();
+// Middleware para analizar cuerpos JSON
+app.use(express.json());
 
+// Establecer el puerto y verificar_token
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
 
-// Middleware para analizar cuerpos JSON 
-app.use ( express.json ( ) ) ;
-
-// Establecer el puerto y verificar_token const port = process.env.PORT || 3000 ; const verifyToken = process.env.VERIFY_TOKEN ;​​​
- 
-
-
-// Ruta para solicitudes GET app.get 
-( ' /' , ( req , res ) = > { const { ' hub.mode' : mode , ' hub.challenge ' : challenge , 'hub.verify_token' : token } = req.query ;   
-       
-
-  si ( modo === 'suscribirse' && token === verifyToken ) { 
-    console.log ( 'WEBHOOK VERIFICADO' ) ; res.status (200) .send ( desafío ) 
-    ; } de lo contrario { res.status ( 
-    403 ) .end ( ) ; } } ) ;    
-    
+// Ruta para solicitudes GET
+app.get('/', (req, res) => {
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
   
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFICADO');
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).end();
+  }
+});
 
+// Ruta para solicitudes POST
+app.post('/', (req, res) => {
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+  console.log(`\n\nWebhook recibido ${timestamp}\n`);
+  console.log(JSON.stringify(req.body, null, 2));
+  res.status(200).end();
+});
 
-// Ruta para solicitudes POST app.post 
-( ' /' , ( req , res ) => { const timestamp = new Date ( ). toISOString (). replace ( ' T' , ' ' ) .slice ( 0,19 ) ; console.log 
-  ( ` \ n \n Webhook recibido $ { timestamp } \n` ); console.log 
-  ( JSON.stringify ( req.body , null , 2 ) ) ; 
-  res.status ( 200 ) .end ( ); } ) ;   
-        
-
-
-// Inicia la 
-aplicación del servidor . listen ( puerto , () => { 
-  console . log (` \n Escuchando en el puerto $ { puerto } \n `); });   
+// Inicia la aplicación del servidor
+app.listen(port, () => {
+  console.log(`\nEscuchando en el puerto ${port}\n`);
+});
